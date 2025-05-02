7503412574bf597a47601f74cde13f3b660d46e9
Anshika Gupta <143327930+anshikaagupta@users.noreply.github.com>
2025-05-02 22:24:48 +0530

    Time: 0 ms (100%), Space: 18 MB (39.92%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,11 +1,25 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n=len(nums)
-        result=[]
-        for i in range(1<<n):
-            subset=[]
-            for j in range(n):
-                if ( i>>j ) & 1:
-                    subset.append(nums[j])
-            result.append(subset)
-        return result        
+        Ans = []
+        n = len(nums)
+
+        # Helper function
+        def Helper(op, start_index):
+            # base case
+            if start_index == n:
+                Ans.append(list(op))
+                return
+            
+            # recursive case
+            # choice 1: include the current element
+            op.append(nums[start_index])
+            Helper(op, start_index + 1)
+            
+            # backtracking step
+            op.pop()
+            
+            # choice 2: exclude the current element
+            Helper(op, start_index + 1)
+        
+        Helper([], 0)
+        return Ans     
