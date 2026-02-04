c33e397d09558a69a330409b7a26b79053d07bb5
Anshika Gupta <143327930+anshikaagupta@users.noreply.github.com>
2026-02-04 21:24:57 +0530

    Time: 3 ms (51.93%), Space: 20.8 MB (10.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mp={}
-        for i, num in enumerate(nums):
-            comp=target-num
-            if comp in mp:
-                return (mp[comp], i)
-            mp[num]=i
-        return [-1,-1]
+        num = [(num, idx) for idx, num in enumerate(nums)]
         
+
+        num.sort(key=lambda x:x[0])
+        n=len(nums)
+        
+        left,right=0,n-1
+        while left<right:
+            cursum=num[left][0]+num[right][0]
+            if cursum==target:
+                return [num[left][1], num[right][1]]
+
+            elif cursum<target: 
+                left+=1
+            else:
+                right-=1
+        return [-1,-1]
